28f2ab0d3647a44ef86ec07a32d0c842fdeb7c33
satrain <mladen.trickovic5@gmail.com>
2021-03-11 19:04:58 +0100

    responsive update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 // navigation on scroll effect
 const header = document.querySelector("header")
+const mediaQuery = window.matchMedia('(max-width: 425px)')
+
+let headerHeight;
 
 let prevScrollpos = window.pageYOffset;
 window.addEventListener("scroll", function () {
     let currentScrollPos = window.pageYOffset;
     if (currentScrollPos < 20) {
+        if(mediaQuery.matches) {
+            headerHeight = "100px";
+        }
+        else {
+            headerHeight = "132px";
+        }
         document.querySelector("header").style.background = "transparent";
-        document.querySelector("header").style.height = "132px";
+        document.querySelector("header").style.height = headerHeight;
         document.querySelector("header").style.transition = ".3s";
     } else {
         document.querySelector("header").style.background = "#fff";
